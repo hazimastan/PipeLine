7fcc708a12b3c7d3d4e165d7533869eb9a992c42
hazimastan <40984308+hazimastan@users.noreply.github.com>
2021-06-29 00:19:50 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,4 +1,4 @@
-def call(){
+def call() {
   //checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/hazimastan/TestPyproject.git']]])
   echo "test"
 }
