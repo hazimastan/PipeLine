1461ab4e2af1c5a3bcb3da779e7c876391b70c04
hazimastan <40984308+hazimastan@users.noreply.github.com>
2021-06-30 13:08:18 +0530

    Delete libs directory

--- libs/checkout.groovy
+++
@@ -1,3 +0,0 @@
-def call(){
-  checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/hazimastan/TestPyproject.git']]])                       
-}
